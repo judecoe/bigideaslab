c857ea0e6d00a721a5041396a063a58e6f534e81
Jude <96027417+judecoe@users.noreply.github.com>
2025-02-25 14:38:23 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+futureAge = age + 10
+
+print("Hello " + name + " in 10 years, you will be " + str(futureAge) + " years old!")
